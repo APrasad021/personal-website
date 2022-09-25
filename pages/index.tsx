5b3f941bb1b759a4766a7eb8e1eb0905db13ec5b
Ashwin Prasad <APrasad021@gmail.com>
2022-09-25 01:18:26 -0700

    Styling fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import NavBarWrapper from '../components/NavBar';
+import Head from 'next/head';
 
-import styles from "../styles/home.module.css";
+import styles from "../styles/links.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p>Hello</p>
+      <Head>
+          <title>Ashwin Prasad</title>
+      </Head>
     </div>
       
   )
